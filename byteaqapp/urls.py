337cfa1b2989ffa41287a3fd6006d85ac995fee4
Taylor Galloway <gallowaytaylor@outlook.com>
2023-12-18 08:05:24 +0700

    Initial commit

---
+++ byteaqapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("/shorten", views.shorten, name="shorten"),
+    path("/expand", views.expand, name="expand"),
+]
