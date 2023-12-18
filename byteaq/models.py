1854d0e3a30c5d9724148ddf9e678004dc585d4f
Taylor Galloway <gallowaytaylor@outlook.com>
2023-12-18 20:05:44 +0700

    Add url model

--- byteaq/models.py
+++ byteaq/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Url(models.Model):
+    short_url = models.CharField(max_length=200, unique=True)
+    long_url = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.short_url
