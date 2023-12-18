cd2bcddcc361209a7ae6411b5fec6020c1bcf370
Taylor Galloway <gallowaytaylor@outlook.com>
2023-12-18 20:06:08 +0700

    Add shrinkify route

--- byteaq/urls.py
+++ byteaq/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name="byteaq"
+
 urlpatterns = [
     path("", views.index, name="index"),
-    # path("/shorten", views.shorten, name="shorten"),
-    # path("/expand", views.expand, name="expand"),
+    path("shrinkify/", views.shrinkify, name="shrinkify"),
+    path("expand/", views.expand, name="expand"),
 ]
--- byteaq/views.py
+++ byteaq/views.py
@@ -1,7 +1,32 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from django.template import loader
+from .models import Url
+import hashlib
 
 def index(request):
     return render(request, "byteaq/index.html")
-    # return HttpResponse("Hello, world. You're at the byteaq index.")
+
+def shrinkify(request):
+    print(f'REQUEST {request.POST["shrinkify"]}')
+    url = request.POST["shrinkify"]
+    hash_object = hashlib.new("shake_256")
+    hash_object.update(url.encode("UTF-8"))
+    hashed_string = hash_object.hexdigest(4)
+    u, created = Url.objects.get_or_create(
+        short_url=f"http://byteaq.com/{hashed_string}", 
+        defaults={
+            "long_url": url
+        }
+    )
+
+    if created:
+        print(f"Object with name '{hashed_string}' was created.")
+    else:
+        print(f"Object with name '{u}' already exists.")
+    
+    print(f'url:{url} HASHED STRING: {hashed_string} created: {created}')
+    return HttpResponse(hashed_string)
+
+def expand(request):
+    return HttpResponse("Hello, world. You're at expand")
