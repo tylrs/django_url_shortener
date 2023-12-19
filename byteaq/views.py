87bc8c045957f96173981f8b49cc86fdb8651536
Taylor Galloway <gallowaytaylor@outlook.com>
2023-12-19 17:15:22 +0700

    Remove print statements

--- byteaq/views.py
+++ byteaq/views.py
@@ -9,7 +9,6 @@ def index(request):
     return render(request, "byteaq/index.html")
 
 def shrinkify(request):
-    # print(f'1) REQUEST {request.POST["shrinkify"]}')
     url = request.POST["shrinkify"]
     hash_object = hashlib.new("shake_256")
     hash_object.update(url.encode("UTF-8"))
@@ -23,7 +22,6 @@ def shrinkify(request):
     )
     
     while not created:
-        # print(f"2) Object with name '{hashed_string}' already exists.")
         characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
         random_char = random.choice(characters)
         hashed_string += random_char
@@ -34,26 +32,19 @@ def shrinkify(request):
                 "long_url": url
             }
         )
-        # print(f'End of loop {u} {created}')
         
-    
-    print(f'url:{url} HASHED STRING: {hashed_string} created: {created}')
-    print(f'LOOK HERE>>> {u} {u.id}')
-    # print(f'{reverse("byteaq:shrinkify_results", args=(u.id))}')
     return HttpResponseRedirect(reverse("byteaq:shrinkify_results", args=(u.id,)))
 
 def shrinkify_results(request, url_id):
     url = get_object_or_404(Url, id=url_id)
     context = {"url": url}
+    
     return render(request, "byteaq/shrinkify_results.html", context)
 
 
 def expand(request):
-    print(f'1) REQUEST {request.GET["expand"]}')
     short_url = request.GET["expand"]
     url = get_object_or_404(Url, short_url=short_url)
-    print(f'Expand results {url}')
-
-    # return HttpResponse(f'Look at url info here: {url}')
     context = {"url": url}
+
     return render(request, "byteaq/expand_results.html", context)
