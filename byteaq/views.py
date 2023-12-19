fb47751f16a718717d446a93ad6fe7c9ae443064
Taylor Galloway <gallowaytaylor@outlook.com>
2023-12-19 10:53:31 +0700

    Add expand logic and sad path for shrinkify

--- byteaq/views.py
+++ byteaq/views.py
@@ -1,32 +1,48 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-from django.template import loader
+from django.shortcuts import render, get_object_or_404
 from .models import Url
 import hashlib
+import random
 
 def index(request):
     return render(request, "byteaq/index.html")
 
 def shrinkify(request):
-    print(f'REQUEST {request.POST["shrinkify"]}')
+    print(f'1) REQUEST {request.POST["shrinkify"]}')
     url = request.POST["shrinkify"]
     hash_object = hashlib.new("shake_256")
     hash_object.update(url.encode("UTF-8"))
     hashed_string = hash_object.hexdigest(4)
+
     u, created = Url.objects.get_or_create(
         short_url=f"http://byteaq.com/{hashed_string}", 
         defaults={
             "long_url": url
         }
     )
+    
+    while not created:
+        print(f"2) Object with name '{hashed_string}' already exists.")
+        characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        random_char = random.choice(characters)
+        hashed_string += random_char
 
-    if created:
-        print(f"Object with name '{hashed_string}' was created.")
-    else:
-        print(f"Object with name '{u}' already exists.")
+        u, created = Url.objects.get_or_create(
+            short_url=f"http://byteaq.com/{hashed_string}", 
+            defaults={
+                "long_url": url
+            }
+        )
+        print(f'End of loop {u} {created}')
+        
     
     print(f'url:{url} HASHED STRING: {hashed_string} created: {created}')
     return HttpResponse(hashed_string)
 
 def expand(request):
-    return HttpResponse("Hello, world. You're at expand")
+    print(f'1) REQUEST {request.POST["expand"]}')
+    short_url = request.POST["expand"]
+    url = get_object_or_404(Url, short_url=short_url)
+    print(f'Expand results {url}')
+
+    return HttpResponse(f'Look at url info here: {url}')
