88f1849f05e098bf1182afaecab5a884828b29b7
Taylor Galloway <gallowaytaylor@outlook.com>
2023-12-19 18:27:45 +0700

    Increase char limit

--- byteaq/models.py
+++ byteaq/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Url(models.Model):
-    short_url = models.CharField(max_length=200, unique=True)
-    long_url = models.CharField(max_length=200)
+    short_url = models.CharField(max_length=2000, unique=True)
+    long_url = models.CharField(max_length=2000)
 
     def __str__(self):
         return f'short: {self.short_url} long: {self.long_url}'
