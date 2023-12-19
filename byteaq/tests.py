8eff981bd0edc66dd5b4d7f0200f3da77b6178ae
Taylor Galloway <gallowaytaylor@outlook.com>
2023-12-19 12:53:54 +0700

    Add outline of tests for expand

--- byteaq/tests.py
+++ byteaq/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from .models import Url
+from django.urls import reverse
 
-# Create your tests here.
+class ExpandViewTests(TestCase):
+    def test_no_matching_short_url(self):
+        """
+        If no matching short url is found it should return 404
+        """
+        url = reverse("byteaq:expand")
+        params = {"expand": "https://byteaq.com/c002178"}
+        print(f'LOOK AT URL>> {url} PARAMS: {params}')
+        response = self.client.get(url, params)
+        print(f'RESPONSE>>>{response}')
+        self.assertEqual(response.status_code, 404)
+        # self.assertContains(response, "No polls are available.")
+        # self.assertQuerySetEqual(response.context["latest_question_list"], [])
+
+    def test_matching_short_url(self):
+        """
+        If a matching short url is found it should return the matching url
+        """
+        url = Url.objects.create(
+            short_url="https://byteaq.com/c002178",
+            long_url="https://example.com"
+        )
+        params = {"expand": "https://byteaq.com/c002178"}
+        response = self.client.get(reverse("byteaq:expand"), params)
+        self.assertContains(response, "c002178")
+        # self.assertQuerySetEqual(
+        #     response.context["latest_question_list"],
+        #     [url],
+        # )
