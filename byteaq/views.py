416860c4fe150c61bcc5697790a3e8783ee7ce44
Taylor Galloway <gallowaytaylor@outlook.com>
2023-12-19 17:46:46 +0700

    Extract generate hash as separate function

--- byteaq/views.py
+++ byteaq/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from .models import Url
@@ -10,9 +10,7 @@ def index(request):
 
 def shrinkify(request):
     url = request.POST["shrinkify"]
-    hash_object = hashlib.new("shake_256")
-    hash_object.update(url.encode("UTF-8"))
-    hashed_string = hash_object.hexdigest(4)
+    hashed_string = generate_hash_from_url(url)
 
     u, created = Url.objects.get_or_create(
         short_url=f"https://byteaq.com/{hashed_string}", 
@@ -22,9 +20,9 @@ def shrinkify(request):
     )
     
     while not created:
-        characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-        random_char = random.choice(characters)
-        hashed_string += random_char
+        chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        random_char = random.choice(chars)
+        hashed_string = generate_hash_from_url(url + random_char)
 
         u, created = Url.objects.get_or_create(
             short_url=f"https://byteaq.com/{hashed_string}", 
@@ -38,7 +36,7 @@ def shrinkify(request):
 def shrinkify_results(request, url_id):
     url = get_object_or_404(Url, id=url_id)
     context = {"url": url}
-    
+
     return render(request, "byteaq/shrinkify_results.html", context)
 
 
@@ -48,3 +46,8 @@ def expand(request):
     context = {"url": url}
 
     return render(request, "byteaq/expand_results.html", context)
+
+def generate_hash_from_url(url):
+    hash_object = hashlib.new("shake_256")
+    hash_object.update(url.encode("UTF-8"))
+    return hash_object.hexdigest(4)
